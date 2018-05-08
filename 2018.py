348563b0269b16e55a72b0be5df12f0853e2986b
Stephen Byers <byerss@gmail.com>
2018-05-08 07:44:51 -0700

    Add adafruit-io and basic error handling.

--- 2018.py
+++ 2018.py
@@ -5,13 +5,14 @@ import subprocess
 import time
 import datetime
 import requests
-import time
 import MySQLdb as my
+from Adafruit_IO import Client
+
 
 UPDATETIME = 60
-
-
 lastValue = 0
+ADAFRUIT_IO_KEY = 'XXXX'
+aio = Client(ADAFRUIT_IO_KEY)
 
 
 # function to read the temperature from ds18b20 temperature sensor on i2c
@@ -26,6 +27,9 @@ def read_temperature():
    temperature = "{0:.2f}".format(temperature)
    crcResult = thetext.split("\n")[0].split(" ")[11]
    return temperature, crcResult
+
+
+
 
 
 def start():
@@ -43,11 +47,15 @@ def start():
         time.sleep(2)
 
 
+
+
 # main program entry point - runs continuously updating our datastream with the
 # latest temperature reading
 def run():
   global lastValue
   start()
+
+
 
   while True:
     degreesF, crcResult = read_temperature()
@@ -77,26 +85,34 @@ def run():
          print("Looks good!")
          lastValue = floatTemp
          print("Update Last Value")
+         try:
+           db = my.connect(host="192.168.1.22",
+           user="rpi",
+           passwd="olpride",
+           db="test"
+           )
+           cursor = db.cursor()
+           sql = "insert into temperature VALUES(now(), '%s')" % (floatTemp)
+           number_of_rows = cursor.execute(sql)
+           db.commit()   #Needed to commit changes
+           db.close()
+         except:
+           pass
+         
+         try:
+           aio.send('Pool_Monitor', floatTemp)
+         except:
+           pass
 
-         db = my.connect(host="192.168.1.22",
-         user="rpi",
-         passwd="olpride",
-         db="test"
-         )
-         cursor = db.cursor()
-         sql = "insert into temperature VALUES(now(), '%s')" % (floatTemp)
-         number_of_rows = cursor.execute(sql)
-         db.commit()   #Needed to commit changes
-         db.close()
-         time.sleep(UPDATETIME)
-
+         time.sleep(60)
+         #quit()
     else:
      print("crc Result: NO")
      time.sleep(1)    
 
 
+
+
+
+
 run()
-
-
-
-
